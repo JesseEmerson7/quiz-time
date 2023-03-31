4cb06d31de03b62d503087037651a4d02dd63e9d
Jesse Emerson <jesseemerson7@gmail.com>
2023-03-30 23:45:38 -0400

    starting on js

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+let QIndex = 0;
+
+let questions = [
+{
+    q:"What is the proper function syntax?",
+    answers:["func(){};","function{}[];","!func(){}:","function(){};"],
+    a:"function(){};"
+
+},
+{
+
+    q:"",
+    answers:[],
+    a:""
+},
+{
+    q:"",
+    answers:[],
+    a:""
+
+},
+{
+    q:"",
+    answers:[],
+    a:""
+
+},
+
+
+]
