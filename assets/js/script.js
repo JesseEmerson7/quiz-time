58b5464b4030b7e3181f19cdf5d08c2785d0e61b
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-01 19:55:50 -0400

    got javascript set up and start button workin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 let QIndex = 0;
 
-let questions = [
+let time = 70;
+
+
+const questions = [
 {
     q:"What is the proper function syntax?",
     answers:["func(){};","function{}[];","!func(){}:","function(){};"],
@@ -14,17 +17,65 @@ let questions = [
     a:"Selects a CSS selector"
 },
 {
-    q:"",
-    answers:[],
-    a:""
+    q:"How do you format an if statement?",
+    answers:["if then(){maybe}","if(){}then","then if (){}","if we do this then{}"],
+    a:"if(){}then"
 
 },
 {
-    q:"",
-    answers:[],
-    a:""
+    q:"How would you stop event bubbling?",
+    answers:["Tell it to stop",".stopBubble",".stopPropagation",".stopNow"],
+    a:".stopPropagation"
 
 }
+];
+
+const startBtn = document.getElementById('start-btn');
+
+const startQuizDiv = document.getElementById('start-quiz')
+
+const questionDiv = document.getElementById('question-div')
+
+const questionBox = document.getElementById('question')
+
+const answerOne = document.getElementById('answer-1')
+
+const answerTwo = document.getElementById('answer-2')
+
+const answerThree = document.getElementById('answer-3')
+
+const answerFour = document.getElementById('answer-4')
+
+const rightWrong = document.getElementById('right-wrong')
+
+const timer = document.getElementById('timer')
 
 
-]
+
+
+
+
+startBtn.addEventListener('click', startGame);
+startBtn.addEventListener('click', timerFunction);
+
+
+
+function startGame(){
+   timer.innerText = time;
+   
+   startQuizDiv.classList.add('hidden');
+   questionDiv.classList.remove('hidden');
+
+
+
+
+
+
+
+
+
+};
+
+function timerFunction(){
+console.log('timer starts too');
+};
